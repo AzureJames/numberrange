8924bdfe6f8293cfd903e2272177d819f1431f30
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2023-12-28 00:17:36 -0700

    dec28

--- lotoReg3custom.js
+++ lotoReg3custom.js
@@ -77,7 +77,7 @@ submitForm.addEventListener("click", () => {
     colOne.push(regularData[q].evening_daily[0]); //ARRAYS FROM THE RAW DATA0
     colTwo.push(regularData[q].evening_daily[1]);
     colThree.push(regularData[q].evening_daily[2]);
-    if(q == lastRegDay-1){outputs.innerHTML += "<p>Farthest time: " + regularData[q].draw_date + " Oldest loto numbers: " + regularData[q].midday_daily + regularData[q].evening_daily + "</p>";}
+    if(q == lastRegDay-1){outputs.innerHTML += "<p>Oldest time: " + regularData[q].draw_date + " Oldest loto numbers: " + regularData[q].midday_daily + regularData[q].evening_daily + "</p>";}
   }
     //console.log("grpAry",regGroupArray);
 
