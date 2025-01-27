1b2b575e54108efbe535e0fa64de382d37d51199
Azure James G.M. <77216235+AzureJames@users.noreply.github.com>
2025-01-26 21:36:00 -0700

    Update loto4cal.js

--- loto4cal.js
+++ loto4cal.js
@@ -114,7 +114,7 @@ submitForm.addEventListener("click", () => {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   var firstMonth = parseInt(startForm.value.substring(6,9))-1
   firstMonth = firstMonth.toString()
-  var secondMonth = parseInt(startForm.value.substring(6,9))-1
+  var secondMonth = parseInt(endForm.value.substring(6,9))-1
   secondMonth = secondMonth.toString()
 
   console.log(startForm.value)
