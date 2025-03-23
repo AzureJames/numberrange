aee6e002ec444b66063d28cc3045974ba009eacb
Azure James G.M. <77216235+AzureJames@users.noreply.github.com>
2025-03-23 19:26:30 -0400

    Update lotoReg3custom.js underline

--- lotoReg3custom.js
+++ lotoReg3custom.js
@@ -212,13 +212,13 @@ console.log(colThree);
         tboutputsNormed.innerHTML += "<p>";
         tboutputsNormed.innerHTML += "<span style='color:purple' > " + i + " </span>";
         if (occurenceOf[i] > averageCol1 - 3 && occurenceOf[i] < averageCol1 + 3){tboutputsNormed.innerHTML += "<span> " + occurenceOf[i] + " </span>";}
-        else {tboutputsNormed.innerHTML += "<span>  </span>";}
+        else {tboutputsNormed.innerHTML += "<span>__ </span>";}
 
         if (occurenceOfCol2[i] > averageCol2 - 3 && occurenceOfCol2[i] < averageCol2 + 3){tboutputsNormed.innerHTML += "<span> " + occurenceOfCol2[i] + " </span>";}
-        else {tboutputsNormed.innerHTML += "<span>  </span>";}
+        else {tboutputsNormed.innerHTML += "<span>__ </span>";}
         
         if (occurenceOfCol3[i] > averageCol3 - 3 && occurenceOfCol3[i] < averageCol3 + 3){tboutputsNormed.innerHTML += "<span> " + occurenceOfCol3[i] + " </span>";}
-        else {tboutputsNormed.innerHTML += "<span>  </span>";}
+        else {tboutputsNormed.innerHTML += "<span>__ </span>";}
       //  tboutputsNormed.innerHTML += "<span> " + occurenceOfCol2[i] +  " </span>";
     //    tboutputsNormed.innerHTML += "<span> " + occurenceOfCol3[i] + " </span>";
         var sum = occurenceOf[i] + occurenceOfCol2[i] + occurenceOfCol3[i];
